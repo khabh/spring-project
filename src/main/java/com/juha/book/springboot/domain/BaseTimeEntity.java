4a869bc17e8f2de87340583b2e1db2f42aedf43e
JUHA <khabh@naver.com>
2023-03-08 20:43:00 +0900

    게시판 crud 구현 및 테스트 코드 작성

---
+++ src/main/java/com/juha/book/springboot/domain/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.juha.book.springboot.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass // JPA Entity 클래스들이 BaseTimeEntity를 상속할 경우 필드들을 칼럼으로 인식하도록 함.
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
---
+++ src/main/java/com/juha/book/springboot/web/IndexController.java
@@ -0,0 +1,36 @@
+package com.juha.book.springboot.web;
+
+import com.juha.book.springboot.service.posts.PostsService;
+import com.juha.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto responseDto = postsService.findById(id);
+        model.addAttribute("post", responseDto);
+
+        return "posts-update";
+    }
+}
